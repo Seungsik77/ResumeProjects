faa4a33fee676e96c98ce805b252f885c4193013
Seungsik77 <sungsik124@proton.me>
2025-11-16 20:17:54 -0600

    Delete redundant SQL queries from analysis script
    Removed multiple SQL queries related to layoffs data analysis.

--- LayoffsDataAnalysis(1).sql
+++ LayoffsDataAnalysis(1).sql
@@ -46,40 +46,7 @@ FROM layoffs_staging2
 GROUP BY company
 ORDER BY 2 DESC;
 
-SELECT SUBSTRING(`date`,6,2) AS `MONTH`, SUM(total_laid_off) 
-FROM layoffs_staging2
-WHERE SUBSTRING(`date`,1,7) IS NOT NULL
-GROUP BY `MONTH`
-ORDER BY 1 ASC;
 
-WITH Rolling_Total AS(
-SELECT SUBSTRING(`date`,6,2) AS `MONTH`, SUM(total_laid_off) AS total_off
-FROM layoffs_staging2
-WHERE SUBSTRING(`date`,1,7) IS NOT NULL
-GROUP BY `MONTH`
-ORDER BY 1 ASC)
-SELECT `MONTH`, total_off, SUM(total_off) OVER(ORDER BY `MONTH`) AS rolling_total
-FROM Rolling_Total;
-
-SELECT company, SUM(total_laid_off)
-FROM layoffs_staging2
-GROUP BY company
-ORDER BY 2 DESC;
-
-SELECT company, YEAR(`date`), SUM(total_laid_off)
-FROM layoffs_staging2
-GROUP BY company, YEAR(`date`)
-ORDER BY 3 DESC;
-
-WITH Company_Year (company, years, total_laid_off) AS(
-SELECT company, YEAR(`date`), SUM(total_laid_off)
-FROM layoffs_staging2
-GROUP BY company, YEAR(`date`)
-)
-SELECT *, DENSE_RANK() OVER (PARTITION BY years ORDER BY total_laid_off DESC)
-FROM Company_Year
-WHERE years IS NOT NULL
-ORDER BY RANKING ASC;
 
 
 
